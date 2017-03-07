41900c60bb7d32ee8145b5711a47dcbc867014dd
Alisa Schink <Alisa Schink>
2017-03-07 13:40:14 -0500

    burger model created

--- models/burgers.js
+++ models/burgers.js
@@ -1,3 +1,24 @@
 // imports orm.js
 var orm = require("../config/orm.js");
 
+// calls orm functions using burger-specific input
+var burger = {
+ all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+   create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });  
+  },
+   update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    }); 
+  }
+};
+
+
+module.exports = burger;
