337b32092dc39d5347633f365bcd8e8e6aa98cd1
Alisa Schink <Alisa Schink>
2017-03-06 21:44:41 -0500

    orm.js updated

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,31 @@
 // import MySQL connection.
 var connection = require("../config/connection.js");
 
+// helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+
 // object for all SQL statement functions.
 var orm = {
 
